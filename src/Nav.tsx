1d13c1ea9a8ed0d6d10484a998c72918b7a82ff8
Kirsten <k.m.edwards91@gmail.com>
2025-10-04 13:21:12 -0700

    Getting Nav set up

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,9 +1,14 @@
+import { AssignmentTwoTone, HomeFilled } from "@mui/icons-material"
+
 export const Nav = () => {
     return (
-        <div className="w-screen bg-gray-100">
-            <div className="flex justify-end px-10 py-4">
-                <div>Task Flow</div>
-                <Assign>
+        <div className="w-screen bg-gray-100 flex flex-row justify-between p-2">
+            <div className="flex">
+                <HomeFilled />
+            </div>
+            <div className="flex justify-end space-x-1 underline decoration-2">
+                <div className="font-bold">Task Flow</div>
+                <AssignmentTwoTone color="primary" />
             </div>
         </div>
     )
