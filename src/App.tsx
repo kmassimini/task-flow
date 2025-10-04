058c16b30f859fca7f00226c844b1a2d44e09fe6
Kirsten <kirsten.massimini@gmail.com>
2025-10-04 12:34:50 -0700

    Initial commit - Vite + React + TypeScript + Tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Nav } from './Nav'
+
+function App() {
+
+  return (
+    <>
+      <Nav />
+      <div className='text-blue-600 underline'>
+        Hello
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Nav.tsx
@@ -0,0 +1,10 @@
+export const Nav = () => {
+    return (
+        <div className="w-screen bg-gray-100">
+            <div className="flex justify-end px-10 py-4">
+                <div>Task Flow</div>
+                <Assign>
+            </div>
+        </div>
+    )
+}
